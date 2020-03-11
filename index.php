3e9e0c98aa5965f92dd2c8d07026cd310c9ef661
xiongjialu <35017066+xiongjialu@users.noreply.github.com>
2020-03-11 11:44:10 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,14 +7,16 @@ if($user==null||$pass==null){
     echo "<script>alert('你不是管理员吧！')</script>";
     die("账号和密码不能为空!");
 }
+/*
 function check_param($value=null) {  //过滤注入所用的字符，防止sql注入
     $str = 'select|insert|and|or|update|delete|\'|\/\*|\*|\.\.\/|\.\/|union|into|load_file|outfile';
     if(eregi($str,$value)){
         exit('参数非法！');
     }
     return true;
-}
-if(check_param($user)==true&&check_param($pass)==true){
+}*/
+//if(check_param($user)==true&&check_param($pass)==true){
+if(1){
   $sql='select * from test where user='."'{$user}'and password="."'$pass';";
   $res=mysqli_query($con,$sql);
   $row=$res->num_rows; //将获取到的用户名和密码拿到数据库里面去查找匹配
